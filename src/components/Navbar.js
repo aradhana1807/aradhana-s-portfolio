1f02e4d0490e9f68320ac09f5db475cca40b9a91
aradhana1807 <aradhana.zero@gmail.com>
2024-01-18 10:42:35 +0530

    added mobile-nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 // import '../index.css';
-import '../components/styles/Navbar.css'
-
-import React from 'react'
+import '../components/styles/Navbar.css';
+import menu_logo from './images/menu.png';
+import React, { useState } from 'react'
 import logo_dark from './images/an-red.png';
 import { Link } from 'react-scroll';
 const Navbar = () => {
+    const [showMenu, setshowMenu] = useState(false);
 
     return (
         <nav className='navbar'>
@@ -27,7 +28,33 @@ const Navbar = () => {
                 <Link
                     className='menu-list-item' id='resume-button'>Resume</Link>
             </div>
-        </nav>
+
+
+            {/* mobile-menu */}
+            <img
+                className='mobile-menu-logo'
+                src={menu_logo} alt='menu_log'
+                onClick={() => setshowMenu(!showMenu)}
+            />
+            <div className='mobile-menu-list' style={{ display: showMenu ? 'flex' : 'none' }}>
+                <Link
+                    onClick={() => setshowMenu(false)}
+                    activeClass='active' to='about' spy={true} smooth={true} offset={-150} duration={500}
+                    className='mobile-menu-list-item' >about</Link>
+                <Link
+                    onClick={() => setshowMenu(false)}
+                    activeClass='active' to='experience' spy={true} smooth={true} offset={-150} duration={500}
+                    className='mobile-menu-list-item'>experience</Link>
+                <Link
+                    onClick={() => setshowMenu(false)}
+                    activeClass='active' to='' spy={true} smooth={true} offset={-100} duration={500}
+                    className='mobile-menu-list-item'>contact</Link>
+                <Link
+                    onClick={() => setshowMenu(false)}
+                    className='mobile-menu-list-item' id='mobile-resume-button'>Resume
+                </Link>
+            </div>
+        </nav >
     )
 }
 
